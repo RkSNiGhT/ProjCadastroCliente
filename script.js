e5da19ea295231084a552719d40e9062f640b673
ric <ricgames1@hotmail.com>
2024-11-30 11:36:24 -0300

    closes #3 - Adicionando e resetando o formulario

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 
-
-
+let Clientes = [{
+    id: 1,
+    nome: "Roberto",
+    endereço: "Rua Jacinto",
+    cep: "Indaiatuba - SP"
+}];
+let uf;
 
 inputStart();
 
@@ -14,12 +19,14 @@ function CEPSearch() {
 
     $.getJSON(url, (cep) => {
 
+        uf = cep.uf;
+
         document.getElementById("inputEndere").value = cep.logradouro;
         document.getElementById("inputBairro").value = cep.bairro;
         document.getElementById("inputCidade").value = cep.localidade;
         document.getElementById("inputEstado").value = cep.estado;
 
-        
+
         document.getElementById("areaCEP").innerHTML = ``;
 
         if ("erro" in cep) {
@@ -30,8 +37,8 @@ function CEPSearch() {
             document.getElementById("inputEstado").value = "";
 
             document.getElementById("areaCEP").innerHTML = `<div class="alert alert-danger" role="alert"> CEP invalido! </div>`;
-        }else{
-            document.getElementById("inputNum").innerHTML = `<input id="inputNumero" type="number" class="form-control"></input>`;  
+        } else {
+            document.getElementById("inputNum").innerHTML = `<input id="inputNumero" type="number" class="form-control"></input>`;
         }
 
 
@@ -40,7 +47,48 @@ function CEPSearch() {
     });
 
 }
+
+function add() {
+
+    let cli = {
+        id: Clientes.length + 1,
+        nome: document.getElementById("inputNome").value +" "+ document.getElementById("inputSobrenome").value,
+        endereço: document.getElementById("inputEndere").value,
+        cep: document.getElementById("inputEstado").value + " - " + uf
+    };
+
+    addNewRow(cli);
+    Clientes.push(cli);
+
+    document.getElementById("some-form").reset();
+    document.getElementById("inputNum").innerHTML = `<input id="inputNumero" type="number" class="form-control" disabled></input>`
+    /**const form = document.getElementById('some-form')
+    form.addEventListener('submit', e => {
+        e.preventDefault()
+        console.log('Deu certo')
+    })**/
+
+}
+
 function inputStart() {
     document.getElementById("inputNum").innerHTML = `<input id="inputNumero" type="number" class="form-control" disabled></input>`
 
+    for (let cli of Clientes) {
+        addNewRow(cli);
+    }
+
+}
+
+function addNewRow(cli) {
+    let table = document.getElementById("tableClientes");
+    let newRow = table.insertRow();
+
+    let idNode = document.createTextNode(cli.id);
+    newRow.insertCell().appendChild(idNode);
+    let nomeNode = document.createTextNode(cli.nome);
+    newRow.insertCell().appendChild(nomeNode);
+    let endereçoNode = document.createTextNode(cli.endereço);
+    newRow.insertCell().appendChild(endereçoNode);
+    let cepNode = document.createTextNode(cli.cep);
+    newRow.insertCell().appendChild(cepNode);
 }
